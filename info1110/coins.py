ca28d5703f2a2d02e735e046b126c546e984c884
余留 <yuliumic@outlook.com>
2019-11-08 20:37:47 +1100

    improve coin.py

--- info1110/coins.py
+++ info1110/coins.py
@@ -1,16 +1,8 @@
 x = int(input('input the amount of money: $ '))
 money_ls = [100, 50, 20, 10, 5, 2, 1]
-ls = []
-m_ls = []
 i = 0
 while x != 0:
 	if x //money_ls[i] > 0:
-		ls.append(x//money_ls[i])
-		m_ls.append(money_ls[i])
+		print('{} x ${}'.format(x//money_ls[i], money_ls[i]))
 		x %= money_ls[i]
 	i += 1
-	
-i = 0
-while i < len(ls):
-	print('{} x ${}'.format(ls[i], m_ls[i]))
-	i += 1	
