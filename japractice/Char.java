a1c9c72f4eb5d09a82d951c991fb3e2c5de12fc7
余留 <yuliumic@outlook.com>
2019-12-05 17:25:37 +1100

    practice java

---
+++ japractice/Char.java
@@ -0,0 +1,8 @@
+class Char {
+	public static void main(String[] args) 
+	{
+		String greet = "hello";
+		System.out.println(greet);
+		System.out.print(greet.equalsIgnoreCase("hellO"));
+	}
+}
---
+++ japractice/hello.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.*;
+
+class hello 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello world");
+		System.out.println(Math.sqrt(4));
+	}
+}
