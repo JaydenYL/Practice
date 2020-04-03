647df8c27c1f6bc9055166d4a4bfb1d2d0ba8a41
余留 <yuliumic@outlook.com>
2020-04-03 17:19:06 +1100

    read book

--- Cpractice/3-1.c
+++ Cpractice/3-1.c
@@ -8,6 +8,6 @@ int main(int argc, char *argv[]) {
 	unsigned short int y = 0;
 	printf("y : %lu,  x : %lu\n", sizeof(y), sizeof(x));
 	float z = 40;
-	printf("%.0A", z);
+	printf("%A", z);
 	return 0;
 }
