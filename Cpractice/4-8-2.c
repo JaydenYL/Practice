b6ab4d6cde719aa9240858233073b63ed8a50460
余留 <yuliumic@outlook.com>
2020-04-17 00:08:45 +1000

    C

--- Cpractice/4-8-2.c
+++ Cpractice/4-8-2.c
@@ -4,5 +4,5 @@ int main(int argc, char *argv[])
 {
 	char surname[20] , first[20];
 	scanf("%s %s", first, surname);
-	printf("surname\": %-20s\"\nfirstname: \": %-20s\"", surname, first);
+	printf("%n%nsurname\": %-20s\"\nfirstname: \": %-20s\"", surname, first);
 }
