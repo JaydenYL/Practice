39c8f1b9b247f9d9fe71395c744e3182b5b73161
余留 <yuliumic@outlook.com>
2020-05-18 23:45:35 +1000

    add 9-2-1, 9-3-4

---
+++ Cpractice/9-3-4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+void binary(int n);
+int scan(void);
+
+int main(int argc, char *argv[]) 
+{
+//	int number = scan();
+	binary(scan());
+	return 0;
+	
+}
+
+
+void binary(int n)
+{
+	int r = n % 2;
+	if(n >= 2) binary(n / 2);
+	putchar('0'+r);
+}
+
+
+
+int scan()
+{
+	int num;
+	while (scanf("%d", &num) != 1)
+	{
+		printf("invalid input, please enter an natural number or 'q' to quit: \n");
+	}
+	return num;
+}
