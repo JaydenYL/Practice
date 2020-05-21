6a2b7e817d5cc3bbe88719061aa98f023331da55
余留 <yuliumic@outlook.com>
2020-05-21 23:24:02 +1000

    ...

--- Cpractice/A-6-2.c
+++ Cpractice/A-6-2.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char *argv[]) 
 {
+	
 	double rates[4] = {1.0, 3.0, 8.0, 11.0};
 	const double * pt = rates;
 	double * const conspt = rates;
@@ -12,4 +13,5 @@ int main(int argc, char *argv[])
 	printf("conspt = %f\n", *conspt);
 	printf("rated[2]=%f \n",rates[0]);
 	return 0;
+	
 }
