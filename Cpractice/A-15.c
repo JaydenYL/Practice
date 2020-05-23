868ac203eb6e3847893c880bf8056246ce596a10
余留 <yuliumic@outlook.com>
2020-05-24 07:43:44 +1000

    Create A-15.c

---
+++ Cpractice/A-15.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int zippo[4][3] = {{2,4, 0}, {6,8, 1}, {1, 3, 2}, {5,7, 3}};
+	int array[3] = {4, 8, 12};
+	printf("zippo = %p,  zippo + 1 = %p\n", zippo, zippo + 1);
+	printf("%d\n", zippo == zippo[0]);
+	printf("%d\n", array == array[0]);
+	return 0;
+}
