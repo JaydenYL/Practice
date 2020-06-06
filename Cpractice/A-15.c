0bf70a05f527f692d3951c9c6000d1576eec1007
余留 <yuliumic@outlook.com>
2020-06-07 09:12:54 +1000

    a-15

--- Cpractice/A-15.c
+++ Cpractice/A-15.c
@@ -2,13 +2,16 @@
 
 int main(void)
 {
+
 	
-	int zippo[4][3] = {{2,4, 0}, {6,8, 1}, {1, 3, 2}, {5, 7, 3}};
-	int array[3] = {4, 8, 12};
+	int zippo[4][3] = {{2,4,0}, {6,8, 1}, {1, 3, 2}, {5, 7, 3}};
+//	zippo[0][0] = 4;
 	printf("zippo = %p,  zippo + 1 = %p, zippo[0] = %p\n", zippo, zippo + 1, zippo[0]);
 	printf("%d\n", zippo == zippo[0]);
-	printf("%d\n",  *zippo) ;
-	printf("%d\n", array == array[0]);
+	printf("%d\n", zippo == *zippo);
+	printf("%d\n", *(zippo[0]));
+	printf("%p\n",  *(int*)zippo) ;
 	return 0;
+
 
 }
