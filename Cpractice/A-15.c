8e45091fe68e614d5a4da5b3fef8fd2cefd0d1cf
余留 <yuliumic@outlook.com>
2020-06-07 13:29:45 +1000

    c

--- Cpractice/A-15.c
+++ Cpractice/A-15.c
@@ -8,9 +8,8 @@ int main(void)
 //	zippo[0][0] = 4;
 	printf("zippo = %p,  zippo + 1 = %p, zippo[0] = %p\n", zippo, zippo + 1, zippo[0]);
 	printf("%d\n", zippo == zippo[0]);
-	printf("%d\n", zippo == *zippo);
+	printf("%d\n", zippo + 2 == zippo[2]);
 	printf("%d\n", *(zippo[0]));
-	printf("%p\n",  *(int*)zippo) ;
 	return 0;
 
 
---
+++ Cpractice/A-17.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define ROWS 3
+#define COLS 4
+
+void sum_rows(int ar[][COLS], int rows);
+void sum_cols(int [][COLS], int );
+int sum2d(int (*ar)[COLS], int rows);
+
+int main(int argc, char *argv[]) 
+{
+	int array[ROWS][COLS] = {
+		{2, 4, 6, 8},
+		{3, 5, 7, 9},
+		{12, 10, 8, 6}
+	};
+	
+	sum_rows (array, ROWS);
+	sum_cols(array, ROWS);
+	printf("sum of all elements is  %d\n", sum2d(array, ROWS) );
+
+	return 0;
+}
+
+void sum_cols(int ar[][COLS], int rows)
+{
+	int r, c, total = 0;
+	
+	for (c = 0; r < rows; r++)
+	{
+		total += ar[r][c];
+	}
+	printf("col %d sum : %d\n", c, total);
+}
+
+void sum_cols(int ar[][COLS], int rows)
+{
+	int r, c, total;
+	
+	for (c = 0; r < rwos; r++)
+	{
+		total += ar[r][c];
+	}
+	printf("col %d sum : %d\n", c, total);
+}
