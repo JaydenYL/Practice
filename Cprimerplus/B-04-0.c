f644c532279911acb41562d4bcb12e758304f085
余留 <yuliumic@outlook.com>
2020-07-23 18:38:50 +1000

    update

---
+++ Cprimerplus/B-04-0.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void put1(const char * string);
+int put2(const char *);
+
+int main(int argc, char *argv[]) 
+{
+	char * str = "hello world!";
+	put1(str);
+	put2(str);
+	return 0;
+}
+
+void put1(const char * string)
+{
+	int i = 0;
+	while (string[i] )
+		putchar(string[i++]);
+	putchar('\n');
+}
+
+int put2(const char * string)
+{
+	int i = 0;
+	while (*string)
+	{
+		putchar(* string++);
+		i++;
+	}
+	return i;
+}
