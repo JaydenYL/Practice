b15c17e76a00ede6c4ff334384d6e87f7c39fcde
余留 <yuliumic@outlook.com>
2020-07-23 21:15:18 +1000

    update

--- Cprimerplus/B-05-0.c
+++ Cprimerplus/B-05-0.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void fit (char *, unsigned int);
 
 int main(int argc, char *argv[]) 
 {
+	char mesg[] = "hello world!";
+	
+	puts(mesg);
+	fit(mesg, 8);
+	puts(mesg);
+	puts(mesg+9);
 	
 	
+	return 0;
+	
+}
+
+void fit(char * str, unsigned int size)
+{
+	if (strlen(str) > size)
+		*(str + size) = '\0';
 	
 }
