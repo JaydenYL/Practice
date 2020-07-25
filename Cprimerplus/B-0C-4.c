316b43b75a6bb75a2ff54cb0e7aee5de8174ee5d
余留 <yuliumic@outlook.com>
2020-07-25 18:54:39 +1000

    Update B-0C-4.c

--- Cprimerplus/B-0C-4.c
+++ Cprimerplus/B-0C-4.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+char * pt(char *);
+
+int main(int argc, char *argv[]) 
+{
 	
+}
+
+char * pt(char * str)
+{
+	char * pc = str;
+	while (*pc)
+	{
+		
+	}
 }
