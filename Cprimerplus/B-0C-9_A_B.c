fb624ea4c23bfa6eb5e357dd4e0ffa3a7187db3c
余留 <yuliumic@outlook.com>
2020-07-25 20:22:45 +1000

    update

--- Cprimerplus/B-0C-9_A_B.c
+++ Cprimerplus/B-0C-9_A_B.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 size_t strlength(char *);
+char * question_A(char * );
 
 int main(int argc, char *argv[]) 
 {
-	printf("%zu", strlength("hello world"));
+	char str[] = "h ello world";
+	printf("%zu\n", strlength(str));
+	printf("%p answer of question A: %p", str, question_A(str));
 	return 0;
 }
 
@@ -13,4 +16,10 @@ size_t strlength(char * str)
 	size_t count = 0;
 	for ( ; str[count]; count ++);
 	return count ; 
+}
+
+char * question_A(char * str)
+{
+	while (str) if (*str++ == ' ') return str ;
+	return NULL;
 }
