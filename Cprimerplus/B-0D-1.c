efea0fc6146a0bdc5cb3b1d7d6ce7ab83f44c185
余留 <yuliumic@outlook.com>
2020-07-25 21:42:25 +1000

    Update B-0D-1.c

--- Cprimerplus/B-0D-1.c
+++ Cprimerplus/B-0D-1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+char * input(int n);
+
 int main(int argc, char * argv[])
 {
-	
+	char * str = input(10);
+	puts(str);
+	return 0;
+}
+
+char * input(int n)
+{
+	char str[n];
+	for(int i = 0; i < n; i++)
+	{
+		str[i] = getchar();
+	}
+	return str;
 }
