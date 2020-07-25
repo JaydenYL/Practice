cc505cf502342efa8823b396106623899d1a64b5
余留 <yuliumic@outlook.com>
2020-07-25 20:52:05 +1000

    update

--- Cprimerplus/B-0C-9_A_B.c
+++ Cprimerplus/B-0C-9_A_B.c
@@ -2,16 +2,18 @@
 
 size_t  strlength(char *);
 char * question_A(char *);
-void * question_B(char *);
-int * str_compare(char * , char *);
+void  question_B(char *);
+int  str_compare(char * , char *);
 
 
 int main(int argc, char *argv[]) 
 {
-	char str[] = " h ello world";
+	char str[] = "hello world";
 	printf("%zu\n", strlength(str));
+	printf("%d\n", str_compare("hel", "hel"));
 	printf("address of str:       %p \n\
 answer of question A: %p", str, question_A(str));
+	question_B(str);
 	return 0;
 }
 
@@ -43,18 +45,19 @@ void question_B(char * answer)
 	{
 		puts("not correct, enter the answer again: ");
 	}
-	printf("congratualtions\n");
+	printf("correct, congratualtions!\n");
 }
 
 
 int str_compare(char * str1, char * str2)
 {
-	for ( ; *str1 && *str2 ; str++, str++)
+	for ( ; *str1 && *str2 ; str1++, str2++)
 	{
-		if ( *str != *str2) return 0;
-		else if ( *str1 == *str2 && *str1 == '\0' ) return 1;
-		
-		
+		if ( str1[1]  == str2[1] ) {
+			if (str1[1] == '\0') return 1;
+		}
+		else return 0;
 	}
+	
 	return 0;
 }
