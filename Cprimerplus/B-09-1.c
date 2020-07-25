3013664ffce1b3d6fa976a9423503a00ecc40155
余留 <yuliumic@outlook.com>
2020-07-25 17:59:34 +1000

    update B-09-1.c

--- Cprimerplus/B-09-1.c
+++ Cprimerplus/B-09-1.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	char  input[100], * stop;
 	while (gets(input) && *input)
 	{
-		printf("convert %ld, stop at %s(%d)\n", strtol(input, &stop, 10),stop, *stop);
+		printf("convert %ld, stop at %s(%d) \n", strtol(input, &stop, 10),stop, *stop);
 	}
 	return 0;
 }
