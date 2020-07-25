288c2ed3c5fc71a7b404b0e1d7e0c5bbf81e159c
余留 <yuliumic@outlook.com>
2020-07-25 21:50:31 +1000

    update

--- Cprimerplus/B-0D-1.c
+++ Cprimerplus/B-0D-1.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-char * input(int n);
+char * input(int n, char str[n]);
 
 int main(int argc, char * argv[])
 {
-	char * str = input(10);
+	char str[11] = {[10] = '\0'};
+	input(10, str);
 	puts(str);
 	return 0;
+	
 }
 
-char * input(int n)
+char * input(int n, char str[n])
 {
-	char str[n];
 	for(int i = 0; i < n; i++)
 	{
 		str[i] = getchar();
