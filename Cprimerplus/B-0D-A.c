f8c2689f79f3198611e55deb0a4531eb3caa5ee8
余留 <yuliumic@outlook.com>
2020-07-26 18:30:13 +1000

    update B-0D-A.c

--- Cprimerplus/B-0D-A.c
+++ Cprimerplus/B-0D-A.c
@@ -4,5 +4,5 @@ int main(int argc, char *argv[]) {
 	char str[10][100];
 	int i = -1;
 	while (++i < 10 && gets(str[i])){}
-	
+	puts("A. origin\nB. ASCII\nC. length of whole char *\nD. length of the first word\nE. quit");
 }
