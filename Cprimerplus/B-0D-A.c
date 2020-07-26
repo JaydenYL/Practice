cff7035556b21bd745b0234cc1f835b7ff44b1aa
余留 <yuliumic@outlook.com>
2020-07-26 18:33:06 +1000

    Update B-0D-A.c

--- Cprimerplus/B-0D-A.c
+++ Cprimerplus/B-0D-A.c
@@ -5,4 +5,19 @@ int main(int argc, char *argv[]) {
 	int i = -1;
 	while (++i < 10 && gets(str[i])){}
 	puts("A. origin\nB. ASCII\nC. length of whole char *\nD. length of the first word\nE. quit");
+	while(1)
+	{
+		switch (getchar()) 
+		{
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			default:
+				puts("Invalid input, enter your choice agian: ");
+		}
+		while(getchar()!= '\n') 
+			getchar();
+	}
 }
