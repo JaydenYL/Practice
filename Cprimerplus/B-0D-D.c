8219fd1728fdf60bd378332444609368a30e8d6a
余留 <yuliumic@outlook.com>
2020-07-27 08:04:04 +1000

    update

--- Cprimerplus/B-0D-D.c
+++ Cprimerplus/B-0D-D.c
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 	if (argc < 3) return 1;
 	double base = atof(argv[1]), result = base;
 	int power = atoi(argv[2]);
-	int i = 0;
-	while(i < power) result *= base;
-	printf("%f", result);
+	int i = -1;
+	while(++i < power) result *= base;
+	printf("%.2f\n", result);
 	return 0;
 }
