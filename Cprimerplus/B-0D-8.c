ab38250f0fdaeaba2536d672286cdbefe58f17f6
余留 <yuliumic@outlook.com>
2020-07-26 17:06:10 +1000

    Update B-0D-8.c

--- Cprimerplus/B-0D-8.c
+++ Cprimerplus/B-0D-8.c
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+void str_reverse(char *);
+
 int main(int argc, char *argv[]) 
 {
-	
+	char str[100];
+	gets(str);
+	str_reverse(str);
+	puts(str);
+	return 0;
+}
+
+void str_reverse(char * str)
+{
+	size_t length = strlen(str);
+	for (int i = 0, tmp; i < str/2; i++)
+	{
+		tmp = str[i];
+		str[i] = str[length-i-1];
+		str[length-i-1] = tmp;
+	}
 }
