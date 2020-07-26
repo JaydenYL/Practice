329652654987da05580d9f275dfc9633926b8e0f
余留 <yuliumic@outlook.com>
2020-07-27 09:21:05 +1000

    update

--- Cprimerplus/B-0D-F.c
+++ Cprimerplus/B-0D-F.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) 
 {
-	FILE * fp
+	if (argc < 3)
+	{
+		puts("insufficient argument");
+		return -1;
+	}
+	FILE * fp = fopen("textinput.txt", "r");
+	char str[100];
+	if(! fp)
+	{
+		puts("Error opening file");
+		return 1;
+	}
+	while (fgets(str, 100, fp))
+	{
+		for (int i = 0; str[i];i++){
+			putchar(toupper(str[i]));
+		}
+	}
+	fclose(fp);
+	return 0;
 }
