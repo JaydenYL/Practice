5be6b64203d7f55a2bae497e422b1a7626764b6e
余留 <yuliumic@outlook.com>
2020-07-27 19:53:36 +1000

    Update C-02-0.c

--- Cprimerplus/C-02-0.c
+++ Cprimerplus/C-02-0.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
 void report_count();
-void accumulate(int);
+void accumulate();
 
 int count = 0;
 
 int main(int argc, char *argv[]) 
 {
+	int value;
+	register_t int i;
+	puts("Enter a positive integer (0 to quit): ");
+	while (scanf("%d", &value) && value > 0)
+	{
+		count ++ ;
+		for (i = value; i >= 0; i--)
+		{
+			accumulate();
+		}
+		puts("Enter a positive integer (0 to quit): ");
+	}
 	
+	report_count();
+	return 0;
+}
+
+void accumulate()
+{
+	printf("Loop executed %d times", count);
 }
