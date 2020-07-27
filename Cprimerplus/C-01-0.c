d7d3594f1d9f6433cb7b3e16e6f6302cee1fffc2
余留 <yuliumic@outlook.com>
2020-07-27 12:01:30 +1000

    update

--- Cprimerplus/C-01-0.c
+++ Cprimerplus/C-01-0.c
@@ -2,5 +2,13 @@
 
 int main(int argc, char *argv[]) 
 {
-	
+	int x = 30;
+	while(x++ < 33)
+	{
+		int x  = 100;
+		printf("x in inner block: %d\n", x);
+		x ++;
+	}
+	printf("%d", x);
+	return 0;
 }
