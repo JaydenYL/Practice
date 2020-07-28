3c1890089148a9e6935047d9753a196d8112117b
余留 <yuliumic@outlook.com>
2020-07-28 10:13:09 +1000

    update C-04-0/1.c

--- Cprimerplus/C-04-0.c
+++ Cprimerplus/C-04-0.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+extern int rand1();
+extern void xrand1(unsigned int );
 
 int main(int argc, char * argv[])
 {
+	int count;
+	unsigned int seed;
+	puts("Please enter your choice for seed.\n");
+	while (scanf("%u", &seed))
+	{
+		srand1(seed);
+		for (count = 0; count < 5; count ++)
+			printf("%hd\n", rand1());
+		printf("Please enter your next choice: ");
+	}
 	
+	return 0;
 }
--- Cprimerplus/C-04-1.c
+++ Cprimerplus/C-04-1.c
@@ -1,4 +1,14 @@
+// rand1() , srand1()
 
-int main(int argc, char *argv[]) {
-	
+extern static long int next = 1; 
+
+int rand1(void)
+{
+	next = next*1103515245 + 12345;
+	return (unsigned int ) (next/65536) % 32768;
+}
+
+void srand(unsigned int seed)
+{
+	next = seed;
 }
