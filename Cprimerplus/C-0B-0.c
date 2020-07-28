fce5d1ce36947429643e80513f466aadcb71f929
余留 <yuliumic@outlook.com>
2020-07-28 21:49:30 +1000

    Update C-0B-0.c

--- Cprimerplus/C-0B-0.c
+++ Cprimerplus/C-0B-0.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+void critic(int *);
+
+int main(int argc, char *argv[]) 
+{
+	int  unit[1];
+	printf("How many pounds to a frikin of butter ?\n ");
+	scanf("%d", unit);
+	while(*unit != 56)
+	{
+		critic(unit);
+	}	
+	printf("You must have look it up!\n");
 	
+	return  0;
+}
+
+
+void critic(int * num)
+{
+	printf("Unluckily, chummy, try again.\n");
+	scanf("%d", num);
 }
