4bbfccc65486ccb67e46d80584a791587c3b16c5
余留 <yuliumic@outlook.com>
2020-07-28 14:20:06 +1000

    update

--- Cprimerplus/C-06-0.c
+++ Cprimerplus/C-06-0.c
@@ -8,16 +8,16 @@ int main(int argc, char *argv[])
 	double * db;
 	int number , max ;
 	printf("Enter the maximum length of double array: ");
-	scanf("%f", &max);
-	pd = (double *)malloc(sizeof(double) * max);
-	if (pd == NULL )
+	scanf("%d", &max);
+	db = (double *)malloc(sizeof(double) * max);
+	if (db == NULL )
 	{
 		puts("Fail in allocating memory");
 		exit(EXIT_FAILURE);
 	}
 	puts("Enter the values (q to quit):");
 	int i = 0;
-	while ( i < max && scanf("%d", &db[i]) == 1)
+	while ( i < max && scanf("%lf", &db[i]) == 1)
 	{
 		++i;
 	}
@@ -25,6 +25,8 @@ int main(int argc, char *argv[])
 	
 	for (i = 0; i< number ; i++)
 		printf("%f ", db[i]);
+	
+	free(db);
 		
 	return 0;
 }
