1645fbe104cc05e3c7f495c69a571aeaac6716e7
余留 <yuliumic@outlook.com>
2020-07-28 22:06:15 +1000

    update

--- Cprimerplus/C-0B-4.c
+++ Cprimerplus/C-0B-4.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+static int times = 0 ;
+
+void record_circle_times(void);
+
+
 int main(int argc, char *argv[]) {
+	for (int i = 0; i < 10; i++) record_circle_times();
+	printf("Times : %d\n", times);
 	
+	return 0;
+}
+
+
+void record_circle_times(void)
+{
+	times++;
 }
