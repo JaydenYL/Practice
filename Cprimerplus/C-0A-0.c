325ef11512de18ad8c62d58d544508029e7b29b7
余留 <yuliumic@outlook.com>
2020-07-28 21:37:10 +1000

    update

---
+++ Cprimerplus/C-0A-0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+char color = 'B';
+
+void first(void);
+void second(void);
+
+int main(int argc, char *argv[]) 
+{
+	extern char color;
+	printf("the color in main() is %c.\n", color);
+	first();
+	printf("the color in main() is %c.\n", color);
+	second();
+	printf("the color in main() is %c.\n", color);
+	
+	return 0;
+}
+
+void first(void)
+{
+	char color ;
+	color = 'R';
+	printf("the color in first() is %c.\n", color);
+	
+}
+
+
+void second(void)
+{
+	char color;
+	color = 'G';
+	printf("the color in second() is %c.\n", color);
+}
