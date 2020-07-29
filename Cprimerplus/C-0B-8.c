349e01dfa961dfdc55395239f31405e212472bfd
余留 <yuliumic@outlook.com>
2020-07-29 11:02:07 +1000

    update

---
+++ Cprimerplus/C-0B-8.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int* make_array(int , int );
+void show_array(int , int[*]);
+
+int main(int argc, char *argv[]) 
+{
+	int size, value, * pa;
+	
+	printf("Enter the number of elements: ");
+	scanf("%d", &size);
+	while (size > 0) 
+	{
+		printf("Enter the initial value of the array: ");
+		scanf("%d", &value);
+		pa = make_array(size, value);
+		if (pa)
+			show_array(size, pa);
+			free(pa);
+		printf("Enter tht number of elements( < 1 to quit): ");
+		scanf("%d", &size);
+	}
+	
+	return 0;
+}
+
+
+int * make_array(int n, int value)
+{
+	int * pt = (int *)malloc(n*sizeof(int));
+	for (int i = 0; i < n; i++)
+		pt[i] = value;
+	return pt;
+}
+
+void show_array(int n, int array[n])
+{
+	for(int i = 0; i < n; i++)
+	{
+		printf("%4d%c", array[i], (i%10)? ' ': '\n');
+	}
+	
+}
