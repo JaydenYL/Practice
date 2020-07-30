74ff0fc9b6559843839885a73d87f3f5176a0902
余留 <yuliumic@outlook.com>
2020-07-30 11:46:50 +1000

    Create D-04-0.c

---
+++ Cprimerplus/D-04-0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 40
+
+int main(int argc, char *argv[]) 
+{
+	FILE *fp;
+	char words[MAX];
+	
+	if ((fp = fopen("words.txt", "a+" )))
+	{
+		fprintf(stdout, "Can't open \"words\" file.\n");
+		exit(EXIT_FAILURE);
+	}
+	puts("Enter words to add to the file : press the enter");
+	puts("Key at the begining of a line to terminate.");
+	while (gets(words) != NULL && words[0] != 0)
+		fprintf(fp, "%s", words);
+	
+	
+}
