de703375091a737855f6d132eb3ebf4a170ea727
余留 <yuliumic@outlook.com>
2020-08-01 20:54:01 +1000

    Create D-07-0.c

---
+++ Cprimerplus/D-07-0.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 1024
+#define SLEN 81
+
+void append(FILE * source, FILE * dest);
+
+int main(int argc, char *argv[])
+{
+	
+	
+	
+	return 0;
+}
+
+
+
+void append(FILE * source, FILE * dest)
+{
+	
+}
