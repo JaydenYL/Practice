2c5684066636fcb5d0ed78285743988f7e8ea465
余留 <yuliumic@outlook.com>
2020-08-02 16:39:48 +1000

    update

--- Cprimerplus/D-0B-1.c
+++ Cprimerplus/D-0B-1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) 
 {
+	int ch;
+	long count;
+	fprintf(stdout, "Enter the file name: ");
+	char  filename[50];
+	scanf("%s", filename);
+	FILE * fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		fprintf(stdout, "Fail in openeing the file: %s\n", filename);
+	}
+	while((ch = fgetc(fp)) != EOF)
+	{
+		fputc(ch, stdout);
+		count ++;
+	}
+	fclose(fp);
+	printf("File has %ld characters.\n", count);
+	
+	return 0;
 	
 }
