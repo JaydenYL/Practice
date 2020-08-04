f6ff5dade50d6882bfbdfe8e662155fd7dfb93f7
余留 <yuliumic@outlook.com>
2020-08-05 00:28:54 +1000

    update

---
+++ Cprimerplus/output_colors.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define RED  	"\e[0;31m"
+#define L_RED	"\e[1;31m"
+#define NONE	"\e[0m"
+
+int main(int argc, char *argv[]) 
+{
+	printf("[%2u]" RED "RED " L_RED "L_RED\n" NONE, __LINE__);
+	return 0;
+}
