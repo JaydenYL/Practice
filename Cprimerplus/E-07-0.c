ec6826ad60d6497d5b376bb22e4dc8504f14cdbe
余留 <yuliumic@outlook.com>
2020-08-07 00:20:26 +1000

    update

--- Cprimerplus/E-07-0.c
+++ Cprimerplus/E-07-0.c
@@ -10,7 +10,25 @@ struct funds{
 };
 
 
+double sum(const struct funds);
+
+
 int main(int argc, char *argv[]) 
 {
+	struct funds fd = {
+		"bank of China",
+		10000,
+		"bank fo australia",
+		100000
+	};
+	printf("The sum : %.2lf\n", sum(fd));
 	
+	
+	return 0;
+	
+}
+
+double sum(const struct funds  money)
+{
+	return (money.bankfund + money.savefund);
 }
