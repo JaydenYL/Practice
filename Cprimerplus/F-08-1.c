92d9ef94f045619df0600071b5f918bbe5f98612
余留 <yuliumic@outlook.com>
2020-08-20 03:37:05 +1000

    update

--- Cprimerplus/F-08-1.c
+++ Cprimerplus/F-08-1.c
@@ -5,10 +5,12 @@ int stoi(char *);
 int main(int argc, char *argv[]) 
 {
 	char  strs[33];
-	printf("Enter a binary number: ");
-	while(gets(strs))
+	printf("Enter a binary number(q to quit): ");
+	while( scanf("%s", strs)  )
 	{
+		if (*strs == 'q') break;
 		printf("the result is %d\n", stoi(strs));
+		printf("Enter a binary number(q to quit): ");
 	} 
 	
 	return 0;
