ddd8462d91d861404851d3a413be75d846d2932e
余留 <yuliumic@outlook.com>
2020-08-21 08:07:03 +1000

    Create G-1-0.c

---
+++ Cprimerplus/G-1-0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define sqr(x) ((x)*(x))
+
+
+const int LIM = 40;
+static int array[LIM];
+
+int/**/main(int argc, char *argv[]) 
+{
+	
+	printf("%d", 100/sqr(2));
+	return 0;
+	
+}
