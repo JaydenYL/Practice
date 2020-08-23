7327f4c456e0d00ed80ce4d8fdac2f33a0323263
余留 <yuliumic@outlook.com>
2020-08-23 21:01:43 +1000

    update

--- Cprimerplus/G-11-2.c
+++ Cprimerplus/G-11-2.c
@@ -4,5 +4,13 @@
 
 int main(int argc, char *argv[]) 
 {
+	double x, y;
+	puts("Enter two number: ");
+	while(scanf("%lf %lf", &x, &y) == 2)
+	{
+		printf("The harmonic mean of %6.3f and %6.3f is %6.3f\n", x, y, Har(x, y));
+		puts("Enter two number: ");
+	}
 	
+	return 0;
 }
