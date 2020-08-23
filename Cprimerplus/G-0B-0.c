fe338854798d4500be24c9b69bbb23d60510cc20
余留 <yuliumic@outlook.com>
2020-08-23 14:37:38 +1000

    update

--- Cprimerplus/G-0B-0.c
+++ Cprimerplus/G-0B-0.c
@@ -1,6 +1,25 @@
+//assert 
+
 #include <stdio.h>
+#include <math.h>
+#define NDEBUG
+#include <assert.h>
+
+
 
 int main(int argc, char *argv[]) 
 {
+	double x, y,  z;
+	printf("Enter the coordinate : ");
+	while( scanf("%lf %lf", &x, &y) == 2)
+	{
+		z = x*x - y*y;
+		
+		assert(z >= 0);
+		printf("sqrt(x^2 - y^2) = %lf\n", sqrt(z));
+		fprintf(stdout, "the next coordinate:");
+	}
 	
+	
+	return 0;
 }
