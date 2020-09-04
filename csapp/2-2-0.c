8c65fcd24ee9d8d48bea3369de040824c67781cb
LIU YU <yuliumic@outlook.com>
2020-09-04 21:54:15 +1000

    add files

--- csapp/2-2-0.c
+++ csapp/2-2-0.c
@@ -1,12 +1,12 @@
-//
+//different value after each run
 
 #include <stdio.h>
 
 int main(int argc, char *argv[]) 
 {
-	int a = 0xFEDCBA98 << 32;
-	int b = a >> 36;
-	unsigned c = a >> 40 ;
+	int32_t a = 0xFEDCBA98 << 32;
+	int32_t b = 0xFEDCBA98 >> 36;
+	unsigned c = 0xFEDCBA98u >> 40 ;
 	printf("%0#x\n", a);
 	printf("%0#x\n", b);
 	printf("%0#x\n", c);
