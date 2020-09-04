4535fb8f93a20a193626ca6791326ff97eee83e6
LIU YU <yuliumic@outlook.com>
2020-09-04 21:42:11 +1000

    update

--- csapp/2-2-0.c
+++ csapp/2-2-0.c
@@ -1,6 +1,15 @@
+//
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) 
 {
+	int a = 0xFEDCBA98 << 32;
+	int b = a >> 36;
+	unsigned c = a >> 40 ;
+	printf("%0#x\n", a);
+	printf("%0#x\n", b);
+	printf("%0#x\n", c);
 	
+	return 0;
 }
