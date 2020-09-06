7df4923930da47a4ff1d9f7768e968edb627098e
LIU YU <yuliumic@outlook.com>
2020-09-06 23:47:33 +1000

    Update 2-2-4.c

--- csapp/2-2-4.c
+++ csapp/2-2-4.c
@@ -7,6 +7,10 @@ int main(int argc, char *argv[])
 	show_byte((unsigned char *) & a, sizeof(short));
 	unsigned short b = a;
 	show_byte((unsigned char *) & b , sizeof(short));
+	int c = a;
+	show_byte((unsigned char *) & c, sizeof(int));
+	unsigned  d = b;
+	show_byte((unsigned char *) & d, sizeof(int));
 	
 	return 0;
 }
