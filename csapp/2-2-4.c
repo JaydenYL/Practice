71a55fd01fa292e4af4817b29fccbd7269c02461
LIU YU <yuliumic@outlook.com>
2020-09-06 23:37:01 +1000

    deletee exe

--- csapp/2-2-4.c
+++ csapp/2-2-4.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+#include "show_bytes.h"
+int main(int argc, char *argv[]) 
+{
+	short a = -12345;
+	show_byte((unsigned char *) & a, sizeof(short));
+	unsigned short b = a;
+	show_byte((unsigned char *) & b , sizeof(short));
 	
+	return 0;
 }
