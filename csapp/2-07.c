ac156ae3eb0374f6cbe98c7b8b8bd491d267f0c6
LIU YU <yuliumic@outlook.com>
2020-09-06 10:19:48 +1000

    update

--- csapp/2-07.c
+++ csapp/2-07.c
@@ -5,15 +5,18 @@ int uadd_ok(unsigned , unsigned);
 
 int main(int argc, char *argv[]) 
 {
+	
 	int a = 1 << 31;
 	int b = 1 << 31;
-	
-	
 	printf(uadd_ok(a, b) ? "OK\n" : "Not ok\n");
 	
 	a = UINT_MAX;
 	b = 1;
 	printf(uadd_ok(a, b) ? "OK\n" : "Not ok\n");
+	
+	a = (b = INT_MAX);
+	printf(uadd_ok(a, b) ? "OK\n" : "Not ok\n");
+	
 	
 	return 0;
 	
