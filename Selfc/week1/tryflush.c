229ba4393ec9dc9ab287c51081fc831af6a725d9
JaydenYL <yuliumic@outlook.com>
2020-09-22 21:13:13 +1000

    update

--- Selfc/week1/tryflush.c
+++ Selfc/week1/tryflush.c
@@ -5,9 +5,10 @@ int var;
 
 int main(int argc, char *argv[]) {
 	var  = 10;
-	int n = printf("%d\n", var);
-	fflush(stdout);
-	printf("%d", n);
+//	int n = printf("%d\n", var);
+//	fflush(stdout);
+	printf("%d", 1);
+//	printf("%d", 1);
 //	fflush(stdout);
 	return 0;
 
