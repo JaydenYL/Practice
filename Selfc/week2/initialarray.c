7c03f0553e911b6c781bbe05afb1ea9ac18a339f
JaydenYL <yuliumic@outlook.com>
2020-09-23 05:08:46 +1000

    Update initialarray.c

--- Selfc/week2/initialarray.c
+++ Selfc/week2/initialarray.c
@@ -3,6 +3,7 @@
 int at[100]; // all zero if initialized as global
 
 int main(int argc, char *argv[]) {
+	
 	int arr[100] ;			//random integer if in brackets
 	for(int i=0; i < 100; i++){
 		printf("%d %d\n", arr[i], at[i]);
