e7a5cd0ebcd8fc7623dff0c99509e3ba0702101f
JaydenYL <yuliumic@outlook.com>
2020-09-23 05:07:28 +1000

    update

--- Selfc/week2/initialarray.c
+++ Selfc/week2/initialarray.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+int at[100]; // all zero if initialized as global
+
 int main(int argc, char *argv[]) {
-	int arr[100] ;
+	int arr[100] ;			//random integer if in brackets
 	for(int i=0; i < 100; i++){
-		printf("%d ", arr[i]);
+		printf("%d %d\n", arr[i], at[i]);
 	}
 	return 0;
 	
