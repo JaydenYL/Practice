3d016ee082b440d817bf77786ef39ecb22e00acf
JaydenYL <yuliumic@outlook.com>
2020-09-26 09:56:32 +1000

    update

--- Selfc/week3/pass_gen.c
+++ Selfc/week3/pass_gen.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	for(int i = 0; i < len; i++)
 	{
 		
-		while((ch = rand() >> 8) > 32 && ch < 127 )
+		while((ch = rand()) > 32 && ch < 127 )
 		{
 			break;
 		}
