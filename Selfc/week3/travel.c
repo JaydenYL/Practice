0446009be39b0a1513c7ac409ec7eac64d874f2a
JaydenYL <yuliumic@outlook.com>
2020-09-26 09:22:51 +1000

    add Q-4 travel.c

---
+++ Selfc/week3/travel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+# define MPH 0.621371
+
+int main(int argc, char *argv[]) {
+	double speed;
+	double hours;
+	
+	puts("What's your current speed(km/h): ");
+	scanf("%lf", &speed);
+	puts("How many hours are you travelling for: ");
+	scanf("%lf", &hours);
+	printf("You will cover : %.2lf km ( %.2lf mi)\n", speed*hours, speed*hours*MPH);
+	printf("While traveling at %.2lf km/h ( %.2lf mph)\n", speed, MPH * speed);
+	return 0;
+	
+}
