135e6dd30045810db3efe3e1c7a4e16e148d234b
JaydenYL <yuliumic@outlook.com>
2020-09-26 09:50:29 +1000

    update pas_gen

--- Selfc/week3/pass_gen.c
+++ Selfc/week3/pass_gen.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 	size_t len = atoi(argv[1]);
 	char passw[len];
+	time_t t;
+	srand((unsigned int) time(&t));
+	char ch;
+	for(int i = 0; i < len; i++)
+	{
+		
+		if(!((ch = rand() >> 8) > 32 && ch < 127 )){
+			continue;
+		}
+		passw[i] = ch;
+		
+	}
+	for(int i = 0; i < len; i++)
+	{
+		putchar(passw[i]);
+	}
+	putchar(10);
 	
+	return 0;
 }
