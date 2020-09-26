c32d0a01a82d20da8e0413279cf90f80f2a1b77f
JaydenYL <yuliumic@outlook.com>
2020-09-27 01:45:24 +1000

    add out3.c

--- Selfc/week3/out3.c
+++ Selfc/week3/out3.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
+// implemented by fgets
+// fgets(char * restrict str, int size, FILE * restrict stream)
 
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) 
+{
+	char buff[128] ;
+	bzero(buff, 128);
+	FILE * f = fopen(argv[1], "r");
+	while(!feof(f))
+	{
+		fgets(buff, 127, f);
+		buff[127] = 0;
+		fprintf(stdout, "%s", buff);
+		bzero(buff, 128);
+	}
 	
+	return 0;
 }
