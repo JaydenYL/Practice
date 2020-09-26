288ad495c8ebc3fa5dd36ce9a5b984723d75be1b
JaydenYL <yuliumic@outlook.com>
2020-09-27 01:57:10 +1000

    add strlen.c

--- Selfc/week3/strlen.c
+++ Selfc/week3/strlen.c
@@ -4,11 +4,14 @@ int mestrlen(const char *);
 
 int main(int argc, char *argv[]) 
 {
-	
+	char str1[] = "hello world\n";
+	printf("%d\n", mestrlen(str1) );
 	return 0;
 }
 
-int mestrlen(const char *)
+int mestrlen(const char * string)
 {
-	
+	int i = 0;
+	for( ; string[i]; i++);
+	return i;
 }
