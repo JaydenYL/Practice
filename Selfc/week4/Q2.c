fe538f968c16709f2b4b4a625cf2be22d00eef99
JaydenYL <yuliumic@outlook.com>
2020-09-28 14:51:58 +1000

    update

--- Selfc/week4/Q2.c
+++ Selfc/week4/Q2.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-enum TYPE { FIRE, WATER, FLYING, ROCK, ELECTRIC }; struct pokemon {
-const char* name;
-enum TYPE type; };
+enum TYPE 
+{ 	
+	FIRE, WATER, FLYING, ROCK, ELECTRIC 
+}; 
+
+struct pokemon {
+	const char* name;
+	enum TYPE type; 
+};
 
 
 int main(int argc, char *argv[]) {
-	
+	struct pokemon pikachu = { .type = ELECTRIC, .name = "Pikachu" };
+	printf("%d %s\n", pikachu.type, pikachu.name);
+	return 0;
 }
