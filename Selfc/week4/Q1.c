572413a954d581a43767776c66c72afe489a3348
JaydenYL <yuliumic@outlook.com>
2020-09-28 22:43:29 +1000

    Update Q1.c

--- Selfc/week4/Q1.c
+++ Selfc/week4/Q1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 	printf("%lu\n%lu\n%lu\n", sizeof(short)*8, sizeof(int)*8, sizeof(long )*8);
 	printf("sizeof(char *) : %lu\n", sizeof(char *));
 	
