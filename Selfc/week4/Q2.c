fc2139c6825985b94dfb686d4f723bb6eff04775
JaydenYL <yuliumic@outlook.com>
2021-01-10 14:23:28 +1100

    Update Q2.c

--- Selfc/week4/Q2.c
+++ Selfc/week4/Q2.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-enum TYPE 
+
+
+enum TYPE
 {
 	FIRE, WATER, FLYING, ROCK, ELECTRIC 
 };
+
+
 
 struct pokemon {
 	const char* name;
 	enum TYPE type; 
 };
+
 
 
 int main(int argc, char *argv[]) {
