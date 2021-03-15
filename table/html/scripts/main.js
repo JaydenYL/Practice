b0b3846ff4369f27000d2b73b5950da545ec0dbd
JaydenYL <yuliumic@Outlook.com>
2021-03-16 01:57:00 +1100

    practice html

---
+++ table/html/scripts/main.js
@@ -0,0 +1,13 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'HELLO WORLD';
+
+function multiply(num1, num2) {
+	return result = num1 * num2;
+}
+
+document.querySelector('html').onclick = function() {
+	alert('别戳我，我怕疼。');
+}
+
+multiply(20, 20);
+multiply(0.5, 3);
