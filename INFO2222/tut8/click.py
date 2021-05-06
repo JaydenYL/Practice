44bf4d0a1154d9d21437b87f94dc75f61b57ed6b
JaydenYL <yuliumic@Outlook.com>
2021-05-06 14:30:18 +1000

    Update click.py

--- INFO2222/tut8/click.py
+++ INFO2222/tut8/click.py
@@ -2,11 +2,12 @@ import time
 
 from selenium import webdriver
 
-url = "https://yuliuu.com/Articles"
+
+url = "https://canvas.sydney.edu.au/courses/30588/quizzes/142845/take"
 
 driver = webdriver.Chrome("/Users/yu/Downloads/chromedriver")
 driver.get(url)
-
+time.sleep(20)
 driver.find_element_by_xpath("/html/body/div/p[1]/a/font").click()
 
 time.sleep(10)
