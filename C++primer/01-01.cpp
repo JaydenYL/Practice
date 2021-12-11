284f89acc31d15cfcc66e64cd0ffdaeadd81b2c7
JaydenYL <yuliumic@outlook.com>
2021-12-11 18:00:16 +0800

    add c++ primer

---
+++ C++primer/01-01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	cout<< "Enter two numbers" << endl;
+	int n1 = 0, n2 = 0;
+	cin >> n1 >> n2;
+	cout<< "The sum of n1 and n2 is "<< n1+n2<< endl;
+	return 0;
+}
---
+++ C++primer/01-04.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	cout<< "Enter two numbers" << endl;
+	int n1 = 0, n2 = 0;
+	cin >> n1 >> n2;
+	cout<< "The product of n1 and n2 is "<< n1*n2;
+	cout << "---" << endl;
+	return 0;
+}
