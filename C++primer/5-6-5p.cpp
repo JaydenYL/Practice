fc95297d822aa30a8b00e300ef192583800fb7c2
JaydenYL <yuliumic@outlook.com>
2021-12-14 22:25:59 +0800

    Create 5-6-5p.cpp

---
+++ C++primer/5-6-5p.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+	int a, b ;
+label1:
+	try {
+		cin >> a >> b;
+		
+		if (b == 0) {
+			throw runtime_error("Invalid denominator!\n");
+		}
+	} catch (runtime_error err) {
+		printf("Invalid denominator!\n:");
+		goto label1;
+	}
+	
+	
+	
+	cout << a/b << endl;
+}
